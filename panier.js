8c05b6bf7fc452053e289e97f360ddbaa25aa715
hilarionv <hilarion2004@gmail.com>
2026-05-04 16:45:25 +0000

    rtrtr

---
+++ panier.js
@@ -0,0 +1,17 @@
+
+  
+  
+  
+  const params = new URLSearchParams(window.location.search);
+const id = parseInt(params.get('id'));
+
+const produit = produits.find(p => p.id === id);
+
+document.querySelector('.left img').src = produit.image;
+document.querySelector('.left img').alt = produit.nom;
+document.querySelector('.right .nom').textContent = produit.nom;
+document.querySelector('.right .description').textContent = produit.description;
+document.querySelector('.right .prix').textContent = produit.prix + ' FCFA';
+document.getElementById('btn-acheter').onclick = function() {
+    ajouterAuPanier(produit.id);
+  }
---
+++ script.js
@@ -0,0 +1,44 @@
+const produits = [
+    { id: 1, nom: "Crêpe Nutella", prix: 500, image: "crêpe1.PNG", categorie: "crepes", description: "Lorem"},
+    { id: 2, nom: "Crêpe Confiture", prix: 400, image: "crêpe2.PNG", categorie: "crepes",description: "Lorem"},
+    { id: 3, nom: "Crêpe Nature", prix: 300, image: "crêpe3.PNG", categorie: "crepes",description: "Lorem"},
+    { id: 4, nom: "Jus de Bissap", prix: 300, image: "jus1.PNG", categorie: "jus",description: "Lorem"},
+    { id: 5, nom: "Jus de Gingembre", prix: 300, image: "Jus2.PNG", categorie: "jus",description: "Lorem"},
+    { id: 6, nom: "Jus de Ditakh", prix: 300, image: "jus1.PNG", categorie: "jus",description: "Lorem"},
+  ]
+  
+  
+  
+  
+  function genererCards() {
+    const listeCrepes = document.querySelector('.crêpes .splide__list');
+    const listeJus = document.querySelector('.juslocaux .splide__list');
+  
+    produits.forEach(produit => {
+      const li = document.createElement('li');
+      li.classList.add('splide__slide');
+  
+      li.innerHTML = `
+        <div class="product-card">
+          <img src="${produit.image}" alt="${produit.nom}">
+          <p class="produit">${produit.nom}</p>
+          <p class="prix">${produit.prix} FCFA</p>
+   <a href="panier.html?id=${produit.id}">Voir</a>
+      <a href="javascript:void(0)" onclick="ajouterAuPanier(${produit.id})">Acheter</a>
+        </div>
+      `;
+  
+      if (produit.categorie === 'crepes') {
+        listeCrepes.appendChild(li);
+      } else {
+        listeJus.appendChild(li);
+      }
+    });
+  }
+  let count = 0;
+
+  function ajouterAuPanier(id) {
+    const produit = produits.find(p => p.id === id);
+    count++;
+    document.getElementById('compteur').textContent = count;
+  }
